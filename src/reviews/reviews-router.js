00b25de978ba5cc1508014ad1cc7cd143f7808ba
Josh Barnes <JBarnes49@gmail.com>
2020-09-30 12:04:10 -0700

    completed the assignment! whoo!

--- src/reviews/reviews-router.js
+++ src/reviews/reviews-router.js
@@ -1,21 +1,24 @@
-const express = require('express')
-const path = require('path')
-const ReviewsService = require('./reviews-service')
+const express = require('express');
+const path = require('path');
+const ReviewsService = require('./reviews-service');
+const { requireAuth } = require('../middleware/basic-auth');
 
-const reviewsRouter = express.Router()
-const jsonBodyParser = express.json()
+const reviewsRouter = express.Router();
+const jsonBodyParser = express.json();
 
 reviewsRouter
   .route('/')
-  .post(jsonBodyParser, (req, res, next) => {
-    const { thing_id, rating, text, user_id } = req.body
-    const newReview = { thing_id, rating, text, user_id }
+  .post(requireAuth, jsonBodyParser, (req, res, next) => {
+    const { thing_id, rating, text } = req.body;
+    const newReview = { thing_id, rating, text };
 
     for (const [key, value] of Object.entries(newReview))
       if (value == null)
         return res.status(400).json({
           error: `Missing '${key}' in request body`
-        })
+        });
+
+    newReview.user_id = req.user.id;
 
     ReviewsService.insertReview(
       req.app.get('db'),
@@ -25,9 +28,9 @@ reviewsRouter
         res
           .status(201)
           .location(path.posix.join(req.originalUrl, `/${review.id}`))
-          .json(ReviewsService.serializeReview(review))
+          .json(ReviewsService.serializeReview(review));
       })
-      .catch(next)
-    })
+      .catch(next);
+    });
 
-module.exports = reviewsRouter
+module.exports = reviewsRouter;
